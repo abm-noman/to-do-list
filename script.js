7f1de469bf402f0a01d22f6d1ef272990960cc15
abm-noman <noman15-2839@diu.edu.bd>
2024-05-30 02:33:22 +0600

    DONE

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const addButton = document.querySelector("add-button");
 const emptyImage = document.querySelector("empty-image");
 const todoHtml = document.querySelector("todos");
 let todosJson = JSON.parse(localStorage.getItem("todos")) || [];
+const deleteAllButton = document.querySelector(".delete-all");
+const filters = document.querySelectorAll(".filter");
+let filter = '';
 
 showTodos();
 
@@ -52,3 +55,43 @@ addButton.addEventListener("click", () => {
     }
     addTodo(todo);
 });
+
+function updateStatus(todo) {
+    let todoname = todo.parentElement.lastElementChild;
+    if (todo.checked) {
+        todoname.classList.add("checked");
+        todosJson[todo.id].status = "completed";
+    } else {
+        todoname.classList.remove("checked");
+        todosJson[todo.id].status = "pending";
+    }
+    localStorage.setItem("todos", JSON.stringify(todosJson));
+}
+
+
+function remove(todo) {
+    const index = todo.dataset.index;
+    todosJson.splice(index, 1);
+    showTodos();
+    localStorage.setItem("todos", JSON.stringify(todosJson));
+  }
+  
+  filters.forEach(function (el) {
+    el.addEventListener("click", (e) => {
+      if (el.classList.contains('active')) {
+        el.classList.remove('active');
+        filter = '';
+      } else {
+        filters.forEach(tag => tag.classList.remove('active'));
+        el.classList.add('active');
+        filter = e.target.dataset.filter;
+      }
+      showTodos();
+    });
+  });
+  
+  deleteAllButton.addEventListener("click", () => {
+    todosJson = [];
+    localStorage.setItem("todos", JSON.stringify(todosJson));
+    showTodos();
+  });
